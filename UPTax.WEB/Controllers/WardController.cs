b9fcb23db50cb416ff200bef772e4f6f8ee97aa0
eshohag <eshohag@outlook.com>
2021-02-23 23:15:27 +0600

    ওয়ার্ড তালিকা

---
+++ UPTax.WEB/Controllers/WardController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Services.Description;
+using UPTax.Filter;
+using UPTax.Service.Services.UPDetails;
+
+namespace UPTax.Controllers
+{
+    public class WardController : Controller
+    {
+        #region Global Variables
+        private Message _message = new Message();
+        private readonly IUnionParishadService _unionParishadService;
+
+        #endregion
+
+        #region constructor
+        public WardController(IUnionParishadService unionParishadService)
+        {
+            _unionParishadService = unionParishadService;
+        }
+        #endregion
+
+        // GET: Ward
+        [RapidAuthorization(All = true)]
+        public ActionResult Index(string name, int page = 1, int dataSize = 10)
+        {
+            ViewBag.dataSize = dataSize;
+            ViewBag.page = page;
+            ViewBag.name = name;
+
+            var unionList = _unionParishadService.GetPaged(name, page, dataSize);
+            return View(unionList);
+        }
+    }
+}
