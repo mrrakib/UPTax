06e047ba6c6a0953bf9703e96b46b86cf05cc522
Rakibul Islam Biplob <biplob.bd33@gmail.com>
2021-03-18 18:03:09 +0600

    localization done in bangla date

--- UPTax.WEB/App_Start/BundleConfig.cs
+++ UPTax.WEB/App_Start/BundleConfig.cs
@@ -135,6 +135,7 @@ namespace UPTax
                      "~/Scripts/jquery.unobtrusive-ajax.min.js",
                      "~/Content/assets/libs/air-datepicker/js/datepicker.min.js",
                      "~/Content/assets/libs/air-datepicker/js/i18n/datepicker.en.js",
+                     "~/Content/assets/libs/air-datepicker/js/i18n/datepicker.bn.js",
                      "~/Content/assets/libs/jquery-knob/jquery.knob.min.js",
                      "~/Content/assets/libs/jqvmap/jquery.vmap.min.js",
                      "~/Content/assets/libs/jqvmap/maps/jquery.vmap.usa.js",
