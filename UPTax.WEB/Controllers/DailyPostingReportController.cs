3153690fe8406096aa24714695c2a4bfa12698e9
eshohag <eshohag@outlook.com>
2021-05-10 11:57:55 +0600

    INitial ডেইলি পোষ্টিং রিপোর্ট

---
+++ UPTax.WEB/Controllers/DailyPostingReportController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using UPTax.Helper;
+using UPTax.Service.Services;
+using UPTax.Service.Services.UPDetails;
+
+namespace UPTax.Controllers
+{
+    public class DailyPostingReportController : Controller
+    {
+        private readonly Message _message = new Message();
+        private readonly string _userId = RapidSession.UserId;
+        private readonly int _unionId = RapidSession.UnionId;
+        private readonly IWardInfoService _wardInfoService;
+        private readonly IFinancialYearService _financialYearService;
+
+        public DailyPostingReportController(IWardInfoService wardInfoService, IFinancialYearService financialYearService)
+        {
+            _wardInfoService = wardInfoService;
+            _financialYearService = financialYearService;
+        }
+
+        // GET: DailyPostingReport
+        public ActionResult Index()
+        {
+            ViewBag.WardId = new SelectList(_wardInfoService.GetDropdownItemList(_unionId), "Id", "Name");
+            ViewBag.FinancialYearId = new SelectList(_financialYearService.GetAllForDropdown(), "Id", "Name");
+
+            return View();
+        }
+    }
+}
