fa0be22246527a60db46df14fdc84fc18fb3697e
eshohag <eshohag@outlook.com>
2021-05-22 20:40:36 +0600

    মেসেজ বক্স

---
+++ UPTax.WEB/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using UPTax.Helper;
+
+namespace UPTax.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly Message _message = new Message();
+        private readonly string _userId = RapidSession.UserId;
+        private readonly int _unionId = RapidSession.UnionId;
+        public MessageController()
+        {
+
+        }
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Message/Inbox
+        public ActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
